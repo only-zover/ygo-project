f9c9868ed14350bb3bbfe051691a90210ff47979
José Augusto Gomes Martinho <martinho@alunos.utfpr.edu.br>
2023-09-21 13:29:59 +0000

    add better logic

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <script setup>
 import { ref } from 'vue'
 
-const API_URL = "https://db.ygoprodeck.com/api/v7/cardinfo.php?"
-const CARD = ref([])
-const status = ref("")
+const API_URL = import.meta.env.VITE_BASE_URL
+const NAME_PATH = import.meta.env.VITE_NAME_PATH
+const card = ref({type: ''})
+let cardInput = ''
 
 async function getCard() {
-  const response = (await fetch(`${API_URL}name=Dark Magician`))
-  const data = await response.json()
-  const card = CARD.value = data.data[0]
-  status.value = `\nATK: ${card.atk} / DEF: ${card.def}`
+  try {
+    const response = await fetch(`${API_URL}${NAME_PATH}${cardInput}`)
+    const data = await response.json() 
+    card.value = data["data"][0]
+  } catch (error) {
+    console.error(error)
+  }
 }
 
+function updateCardInput(event) {
+  const inputValue = event.target.value
+
+  if (inputValue.trim() !== '') {cardInput = inputValue}
+  else {cardInput = 'Mekk-Knight Avram'}
+}
 </script>
 
 <template>
   <div>
-    <a href="./page2.vue">To page 2</a>
-  </div>
-  <div>
+    <input id="card-input" type="text" @input="updateCardInput" :value="cardInput">
     <button @click="getCard">Get Card</button>
   </div>
   <div>
-    <h1>{{ CARD.name }}</h1>
-    <h2>{{ CARD.desc }}</h2>
-    <p>{{ status }}</p>
+    <h1>{{ card.name }}</h1>
+    <h2>{{ card.desc }}</h2>
+    <p v-if="card.type.includes('Monster')">ATK: {{ card.atk }} / DEF: {{ card.def }}</p>
+    <p>{{ card.race }} {{ card.type }}</p>
+  </div>
+  <div>
+    <a href="./page2.vue">To page 2</a>
   </div>
 </template>
---
+++ src/components/CardInput.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <input type="text" v-model="inputValue">
+      <button @click="handleButtonClick">Get Card</button>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue'
+  
+  export default {
+    props: {
+      initialValue: String, // Valor inicial do campo de entrada
+    },
+    emits: ['getCardClick'], // Evento emitido quando o botão é clicado
+  
+    setup(props, { emit }) {
+      const inputValue = ref(props.initialValue || '') // Valor inicial do campo de entrada
+  
+      function handleButtonClick() {
+        emit('getCardClick', inputValue.value)
+      }
+  
+      return {
+        inputValue,
+        handleButtonClick,
+      }
+    },
+  }
+  </script>
+  
