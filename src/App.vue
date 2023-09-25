fe1668898a9262b275036927aafc5a75be810cc1
José Augusto Gomes Martinho <martinho@alunos.utfpr.edu.br>
2023-09-25 14:26:36 +0000

    add color to cards

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,19 @@ async function getCardByName() {
 
   cards.value = data["data"] 
 }
+
+function cardClass(card) {
+  const cardTypes = ['Spell', 'Trap', 'Normal', 'Link', 'Fusion', 'Synchro', 'XYZ', 'Pendulum', 'Ritual', 'Token', 'Monster'];
+
+  for (const type of cardTypes) {
+    if (card.type?.includes(type)) {
+      return `${type.toLowerCase()}-card`;
+    }
+  }
+
+  return '';
+}
+
 </script>
 
 <template>
@@ -20,16 +33,69 @@ async function getCardByName() {
     <button class="get-card-btn" @click="getCardByName">Get Card</button>
   </div>
   <div id="card-container">
-  <div v-for="card in cards" :key="card.id" class="card-block">
+  <div v-for="card in cards" :key="card.id" :class="cardClass(card)" class="card-block">
     <h1 class="card-title">{{ card.name }}</h1>
     <p class="card-description">{{ card.desc }}</p>
     <span class="card-type">{{ card.race }} {{ card.type }}</span>
     <p class="card-stat" v-if="card.type?.includes('Monster')">ATK: {{ card.atk }} / DEF: {{ card.def }}</p>
-    </div>
+  </div>
 </div>
 </template>
 
 <style>
+.spell-card {
+  background-image: linear-gradient(to bottom, #4CAF50, #357a38);
+}
+
+.trap-card {
+  background-image: linear-gradient(to bottom, #FF69B4, #FF1493);
+}
+
+.monster-card {
+  background-image: linear-gradient(to bottom, #FF9800, #E65100);
+}
+
+.normal-card {
+  background-image: linear-gradient(to bottom, #FFD700, #FFA500);
+}
+
+.link-card {
+  background-image: linear-gradient(to bottom, #2196F3, #1976D2);
+}
+
+.fusion-card {
+  background-image: linear-gradient(to bottom, #9C27B0, #673AB7);
+}
+
+.synchro-card {
+  background-color: #fff;
+
+}
+
+.synchro-card h1, .synchro-card p, .synchro-card span {
+  color: #000000;
+}
+
+.xyz-card {
+  background-color: #000;
+}
+
+.pendulum-card {
+  background-image: linear-gradient(to bottom, #E65100, #fff, #357a38);
+}
+
+.pendulum-card h1, .pendulum-card p, .pendulum-card span {
+  color: #000000;
+}
+
+.ritual-card {
+  background-image: linear-gradient(to bottom, #87CEEB, #00BFFF);
+}
+
+.token-card {
+  background-color:#666666;
+}
+
 .card-input-container {
   display: flex;
   align-items: center;
@@ -81,7 +147,6 @@ async function getCardByName() {
   padding: 10px;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  background-color: #fff;
   transition: transform 0.2s ease-in-out;
   overflow: hidden;
   position: relative;
@@ -96,31 +161,36 @@ async function getCardByName() {
 .card-title {
   font-size: 1.5rem;
   margin: 0;
-  color: #333;
+  color: #fff;
+  /* Adicione mais estilos de acordo com suas preferências */
 }
 
 .card-description {
   font-size: 1rem;
-  color: #777;
-}
-
-.card-info {
-  position: absolute;
-  bottom: 10px;
-  left: 15px; 
+  color: #fff;
+  /* Adicione mais estilos de acordo com suas preferências */
 }
 
 .card-stat {
   font-size: 1.1rem;
   font-weight: bold;
-  color: #555;
+  color: #fff;
+  /* Adicione mais estilos de acordo com suas preferências */
 }
 
 .card-type {
   font-size: 1.1rem;
-  color: #555;
+  color: #fff;
   position: absolute;
   bottom: 10px;
-  right: 15px; 
+  right: 15px;
+  /* Adicione mais estilos de acordo com suas preferências */
+}
+
+
+.card-info {
+  position: absolute;
+  bottom: 10px;
+  left: 15px; 
 }
 </style>
--- src/components/CardInput.vue
+++ src/components/CardInput.vue
@@ -1,31 +1,23 @@
+  
+<script setup>
+import { ref } from 'vue'
+
+const API_URL = import.meta.env.VITE_BASE_URL
+const NAME_PATH = import.meta.env.VITE_NAME_PATH
+const cards = ref([])
+const cardInput = ref('')
+
+async function getCardByName() {
+  const response = await fetch(`${API_URL}${NAME_PATH}${cardInput.value}`)
+  const data = await response.json()
+
+  cards.value = data["data"] 
+}
+</script>
+
 <template>
-    <div>
-      <input type="text" v-model="inputValue">
-      <button @click="handleButtonClick">Get Card</button>
-    </div>
-  </template>
-  
-  <script>
-  import { ref } from 'vue'
-  
-  export default {
-    props: {
-      initialValue: String, // Valor inicial do campo de entrada
-    },
-    emits: ['getCardClick'], // Evento emitido quando o botão é clicado
-  
-    setup(props, { emit }) {
-      const inputValue = ref(props.initialValue || '') // Valor inicial do campo de entrada
-  
-      function handleButtonClick() {
-        emit('getCardClick', inputValue.value)
-      }
-  
-      return {
-        inputValue,
-        handleButtonClick,
-      }
-    },
-  }
-  </script>
-  
+  <div class="card-input-container">
+    <input class="card-input" type="text" v-model="cardInput">
+    <button class="get-card-btn" @click="getCardByName">Get Card</button>
+  </div>
+</template>  
